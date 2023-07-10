f8740a90359fc61f2737ecba6a2b87f07fe6f439
Thhomas1 <thomasromero10@hotmail.com>
2023-07-10 11:57:37 -0300

    Seguimos

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { BsFillMoonStarsFill } from "react-icons/bs";
+import { AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
 export default function Home() {
   return (
     <div>
@@ -9,22 +10,39 @@ export default function Home() {
         <link rel="icon" href="./favicon.ico"></link>
       </Head>
 
-      <main>
-        <section className="ming-h-screen">
-          <nav>
-            <h1>Thomas Romero</h1>
-            <ul>
+      <main className="bg-white px-10">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl font-burtons">Thomas Romero</h1>
+            <ul className="flex items-center">
               <li>
-                <BsFillMoonStarsFill />
+                <BsFillMoonStarsFill className="cursor-pinter text-2xl" />
               </li>
               <li>
-                <a href="#">Resume</a>
+                <a
+                  className=" bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                  href="#"
+                >
+                  Resume
+                </a>
               </li>
             </ul>
           </nav>
+          <div>
+            <h2>Thomas Romero</h2>
+            <h3>Developer</h3>
+            <p>
+              Freelancer providing services for programming and design content
+              needs. Join me down below and let get cracking!
+            </p>
+          </div>
+          <div>
+            <AiFillLinkedin />
+            <AiFillYoutube />
+          </div>
         </section>
       </main>
     </div>
   );
 }
-// 12:10
+// 23:14
