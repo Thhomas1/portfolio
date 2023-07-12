fbd62a3e117ec31de37acf31f3957f21d1223f2e
Thhomas1 <thomasromero10@hotmail.com>
2023-07-11 21:23:18 -0300

    Keeping home page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
+import Image from "next/image";
+import deved from "../public/dev-ed-wave.png";
 export default function Home() {
   return (
     <div>
@@ -28,21 +30,26 @@ export default function Home() {
               </li>
             </ul>
           </nav>
-          <div>
-            <h2>Thomas Romero</h2>
-            <h3>Developer</h3>
-            <p>
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium">
+              Thomas Romero
+            </h2>
+            <h3 className="text-2xl py-2">Developer</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">
               Freelancer providing services for programming and design content
               needs. Join me down below and let get cracking!
             </p>
           </div>
-          <div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
             <AiFillLinkedin />
             <AiFillYoutube />
+          </div>
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+            <Image src={deved} layout="fill" objectFit="cover" />
           </div>
         </section>
       </main>
     </div>
   );
 }
-// 23:14
+// 30:22
