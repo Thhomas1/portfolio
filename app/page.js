0261a2f84f972612ff3a4370622d44a1b26a0180
Thhomas1 <thomasromero10@hotmail.com>
2023-08-18 15:52:30 -0300

    A

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ import Image from "next/image";
 import deved from "../public/prog.png";
 import design from "../public/complements.gif";
 import code from "../public/codecard.gif";
+import wave from "../public/olita.gif";
 import couch from "../public/couch.gif";
 import web1 from "../public/wolfbuilding.png";
 import web2 from "../public/iphonepage.png";
@@ -35,7 +36,7 @@ export default function Home() {
           <nav className="py-5 mb-12  flex justify-between">
             <ul className="text-cyan-900 flex items-center  md:text-xl dark:text-red-400">
               <li className=" flex cursor-pointer">
-                <FaCloudMoonRain clas
+                <FaCloudMoonRain 
                   onClick={() => setDarkMode(!darkMode)}
                   className="cursor-pinter text-3xl"
                 />
@@ -61,8 +62,10 @@ export default function Home() {
               <span className="text-cyan-600 font-bold dark:text-red-400	">+4</span> años de experiencia en el mundo del desarrollo
             </p>
           </div>
-          <div className="cursor-pointer text-4xl flex justify-center gap-12 text-gray-600">
-            <a href="https://www.linkedin.com/in/thomas-romero-05a508245/">
+          <div className=" text-4xl flex justify-center gap-12 text-gray-600 dark:text-white ">
+            <a
+            
+            href="https://www.linkedin.com/in/thomas-romero-05a508245/">
               <AiFillLinkedin />
             </a>
             <a href="https://www.youtube.com/@thomasromero5600/videos">
@@ -72,8 +75,8 @@ export default function Home() {
               <AiFillGithub />
             </a>
           </div>
-          <div className="flex flex-row-reverse dark:text-white">
-             <UseAnimations className="dark:text-white" animation={arrowDown} size={56} />
+          <div className="flex flex-row-reverse dark:fill-blue-500">
+             <UseAnimations animation={arrowDown} size={60} />
           </div>
           <div className="relative mx-auto bg-gradient-to-b from-blue-200 rounded-full w-80 h-80 mt-10 overflow-hidden md:h-85 md:w-85 dark:from-red-300">
             <Image src={deved} layout="fill" objectFit="cover" />
@@ -82,92 +85,86 @@ export default function Home() {
         <section>
           <div className=" flex flex-wrap md:flex-row">
           <div className=" lg:basis-1/2">
-            <h3 className="text-4xl py-1 dark:text-white">About me</h3>
+            <h3 className="text-4xl py-1 text-cyan-950 font-semibold dark:text-white">About me</h3>
             <p className="justify-self-center	 text-md py-2 leading-8 text-gray-800 dark:text-white">
               Me considero una persona altamente organizada y proactiva,
               con una habilidad para colaborar efectivamente en entornos diversos.
               Disfruto especialmente trabajando en equipos multidisciplinarios,
-              donde puedo articular ideas técnicas de manera concisa y por sobre todo poder aprender de otros desarrolladores
+              donde puedo articular ideas técnicas de manera concisa y buscando constantemente oportunidades 
+              para seguir aprendiendo.
               </p>
               <p className="justify-self-center	 text-md py-2 leading-8 text-gray-800 dark:text-white">
               Mi experiencia en
-              <span className="text-amber-400 font-bold	"> JavaScript </span> y
+              <span className="text-amber-400 font-bold"> JavaScript </span> y
               <span className="text-cyan-300 font-bold	"> TypeScript </span>
               me ha permitido desarrollar soluciones robustas y estar en
               constante aprendizaje en sus actualizaciones.
-              Disfruto colaborar en equipos multidisciplinarios,
-              comunicando ideas técnicas de manera clara, buscando constantemente oportunidades 
-              para seguir aprendiendo.
+              
             </p>
           </div>
-          {/* Poner aca la experiencia y agarrar el tailwind de la otra pagina */}
+          
           <div className="overflow:hidden md:basis-1/2 lg:basis-1/2">
             <div className="overflow relative mx-auto w-100 h-100  md:h-96 md:w-96 lg:mr-4">
             <Image src={code} objectFit="cover" width={500} />
           </div>
           </div>
           </div>
-          <div className='relative my-10 grid grid-cols-[30px_1fr] gap-4'>
-            
-      <div className='flex flex-col items-center gap-2'>
-        <div className='rounded-full bg-primary-50 p-1' />
-        <div className='h-full w-0 border border-primary-50' />
-      </div>
-      <div className='flex flex-col gap-2'>
-        <h2 className='text-xl font-bold text-orange-300'></h2>
-        <span className='text-sm text-orange-200'></span>
-        <span className='realtive right-0 text-orange-200 md:absolute'>
-          
-        </span>
 
-       
-      </div>
-      </div>
-
-
-
-          <div className="lg:flex gap-10">
-            <div className="bg-teal-50  text-center shadow-lg p-10 rounded-xl my-10 dark:bg-red-400 dark:text-white">
-              <Image src={couch} width={150} height={150} />
-              <h3 className="text-lg font-semibold pt-8 pb-2">Herramientas</h3>
-              <p className="py-2">
-                Herramientas que me permiten desarrollar y superar desafíos con
-                creatividad y eficiencia
-              </p>
-              <h4 className="py-4 text-teal-600 font-semibold">Herramientas que uso :)</h4>
-
-              <p className="text-gray-800 py-1 dark:text-white">React</p>
-              <p className="text-gray-800 py-1 dark:text-white">Angular</p>
-              <p className="text-gray-800 py-1 dark:text-white">NodeJS</p>
-              <p className="text-gray-800 py-1 dark:text-white">MySQL</p>
-              <p className="text-gray-800 py-1 dark:text-white">-</p>
-              <p className="text-gray-800 py-1 dark:text-white">Javascript</p>
-              <p className="text-gray-800 py-1 dark:text-white">Typescript</p>
-            </div>
-            <div className=" bg-teal-50  text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white-300">
-              <Image src={design} width={150} height={150} />
-              <h3 className="text-lg font-semibold pt-8 pb-2 mt-5">
-                Complementos
-              </h3>
-              <p className="py-2">
-                Frameworks y otras herramientas que me permiten 
-                ahorrar tiempo de desarrollo
-        
-              </p>
-              <h4 className="py-4 text-teal-600 font-semibold">Herramientas que uso :)</h4>
-              <p className="text-gray-800 py-1">NextJs</p>
-              <p className="text-gray-800 py-1">TailwindCSS</p>
-              <p className="text-gray-800 py-1">GSAP</p>
-              <p className="text-gray-800 py-1">BarbaJs</p>
-              <p className="text-gray-800 py-1">Sass</p>
-              <p className="text-gray-800 py-1">MongoDB</p>
-              <p className="text-gray-800 py-1">Postman</p>
-            </div>
+      <div> 
+        <h3 className="text-4xl text-cyan-950 font-semibold py-1 dark:text-white">Experiencia</h3>
+        <div className='relative my-10 grid grid-cols-[30px_1fr] gap-4 '>
+          <div className='flex flex-col items-center gap-2 '>
+            <div className='rounded-full bg-white-10 p-1' />
+            <div className='border-cyan-800 h-full w-0 border border-dashed dark:border-sky-200	' />
           </div>
+          <div className='flex flex-col gap-2'>
+          
+          <h2 className='text-xl font-bold text-cyan-900 dark:text-red-400'>Desarrollador Junior</h2>
+          <h3 className='text-md font-bold text-cyan-800 dark:text-red-300'>Startnet</h3>
+          <span className='text-sm text-cyan-700 dark:text-red-200'> Agosto 2022 - Actualidad</span>
+          <p className="text-md py-2 leading-8 text-gray-800 dark:text-white max-w-[80ch]">
+              La primera empresa que me brindó la oportunidad de ingresar al mundo laboral.
+              En este primer paso crucial de mi carrera, tengo el privilegio de trabajar
+              junto a un equipo excepcional y aprender de ellos constantemente
+              y aplicar mis habilidades técnicas en situaciones reales. 
+              Esta experiencia marcó el inicio de mi trayectoria 
+              y me permitió adquirir valiosos conocimientos prácticos.</p>
+        </div>
+        </div>
+        <div className="flex items-start w-150 h-100">
+            <Image src={wave} objectFit="cover" width={150} />
+          </div>
+        <div className='relative my-10 grid grid-cols-[30px_1fr] gap-4 '>
+          <div className='flex flex-col items-center gap-2'>
+            <div className='rounded-full bg-primary-50 p-1' />
+            <div className='border-cyan-800 h-full w-0 border border-dashed dark:border-sky-200' />
+          </div>
+          <div className='flex flex-col gap-2'>
+          
+          <h2 className='text-xl font-bold text-cyan-900 dark:text-red-400'> Desarrollador Freelance</h2>
+          <h3 className='text-md font-bold text-cyan-800 dark:text-red-300'>MyDevClub</h3>
+          <span className='text-sm text-cyan-700 dark:text-red-200'> Marzo 2022 - Agosto 2022</span>
+          <p className="text-md py-2 leading-8 text-gray-800 dark:text-white max-w-[80ch]">
+            Con un grupo de amigos desarrollamos un sitio web para una constructora de Quilmes.
+            Utilizamos React, Javascript y MaterialUI.
+            La calidad y la sinergia de este grupo son evidentes en cada proyecto que
+            abordamos. La combinación de habilidades, creatividad y dedicación en nuestro
+            equipo potenció nuestra capacidad para superar desafíos y alcanzar resultados
+            sobresalientes de manera consistente.</p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-white max-w-[80ch]">
+            La combinación de habilidades, creatividad y dedicación en nuestro
+            equipo potenció nuestra capacidad para superar desafíos y alcanzar resultados
+            sobresalientes de manera consistente.</p>
+        </div>
+        </div>
+      </div>
+
+            {/* Poner aca algun diseño picante y agarrar el tailwind  */}
+
         </section>
         <section>
           <div>
-            <h3 className="text-5xl py-1 dark:text-white">Proyectos</h3>
+            <h3 className="pt-32 text-4xl py-1 text-cyan-900 font-semibold dark:text-white">Proyectos</h3>
           </div>
           <div className=" flex flex-col gap-10 mt-20 lg:flex-row lg:flex-wrap lg:m-10">
             <div className="basis-1/3 flex-1 mar">
@@ -290,10 +287,48 @@ export default function Home() {
 
             {/* Seguir aca */}
           </div>
+           <div className="lg:flex gap-10">
+            <div className="bg-teal-50  text-center shadow-lg p-10 rounded-xl my-10 dark:bg-red-400 dark:text-white">
+              <Image src={couch} width={150} height={150} />
+              <h3 className="text-lg font-semibold pt-8 pb-2">Herramientas</h3>
+              <p className="py-2">
+                Herramientas que me permiten desarrollar y superar desafíos con
+                creatividad y eficiencia
+              </p>
+              <h4 className="py-4 text-teal-600 font-semibold">Herramientas que uso :)</h4>
+
+              <p className="text-gray-800 py-1 dark:text-white">React</p>
+              <p className="text-gray-800 py-1 dark:text-white">Angular</p>
+              <p className="text-gray-800 py-1 dark:text-white">NodeJS</p>
+              <p className="text-gray-800 py-1 dark:text-white">MySQL</p>
+              <p className="text-gray-800 py-1 dark:text-white">-</p>
+              <p className="text-gray-800 py-1 dark:text-white">Javascript</p>
+              <p className="text-gray-800 py-1 dark:text-white">Typescript</p>
+            </div>
+            <div className=" bg-teal-50  text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white-300">
+              <Image src={design} width={150} height={150} />
+              <h3 className="text-lg font-semibold pt-8 pb-2 mt-5">
+                Complementos
+              </h3>
+              <p className="py-2">
+                Frameworks y otras herramientas que me permiten 
+                ahorrar tiempo de desarrollo
+        
+              </p>
+              <h4 className="py-4 text-teal-600 font-semibold">Herramientas que uso :)</h4>
+              <p className="text-gray-800 py-1">NextJs</p>
+              <p className="text-gray-800 py-1">TailwindCSS</p>
+              <p className="text-gray-800 py-1">GSAP</p>
+              <p className="text-gray-800 py-1">BarbaJs</p>
+              <p className="text-gray-800 py-1">Sass</p>
+              <p className="text-gray-800 py-1">MongoDB</p>
+              <p className="text-gray-800 py-1">Postman</p>
+            </div>
+          </div>
           
         </section>
-        <footer className="mt-5 flex-col text-center text-sky-600 text-sm lg:flex lg:justify-center border-t-2 border-dashed">
-          <div className='text-sky-500 p-10  footer__container'>
+        <footer className="mt-5 flex-col text-center text-sm lg:flex lg:justify-center border-t-2 border-dashed">
+          <div className='text-sky-500 p-10  footer__container dark:text-white'>
           { `Thomas Romero © ${new Date().getFullYear()}` }
           
         </div>
